1d083990cad348245c5ba473538af117b38953b7
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-06 13:14:41 -0200

    criando molde da aplicação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="search">
+          <input type="search" placeholder="Digite o nome do usuario no github"/>
+        </div>
+        <div className="user-info">
+          <img src="https://avatars0.githubusercontent.com/u/25805846?v=4"/>
+          <h1>
+            <a href="https://github.com/ViniciusGularte">Vinicius Gularte</a>
+          </h1>
+          <ul className="repos-info">
+            <li>- Repositorios 34</li>
+            <li>- Seguidores 13</li>
+            <li>- Seguindo 21</li>
+          </ul>
+          <div className="actions">
+            <button>Ver Repositorios</button>
+            <button>Ver Favoritos</button>
+          </div>
+          <div className="repos">
+            <h2>Repositorios: </h2>
+            <ul>
+              <li><a href="">Respositorios...</a></li>
+            </ul>
+          </div>
+          <div className="starred">
+            <h2>Favoritos: </h2>
+            <ul>
+              <li><a href="">Respositorios...</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
