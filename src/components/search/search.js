b029dc4ed6f6f387472618564ecdcd35b20e36a5
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-07 09:21:33 -0200

    Criando Request

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -5,7 +5,22 @@ class Search extends Component {
     return (
       <div className="search">
         <input type="search"
-         placeholder="Digite o nome do usuario no github"/>
+         placeholder="Digite o nome do usuario no github"
+         onKeyUp={(e)=>{
+           const value = e.target.value
+           const keyCode = e.which || e.keyCode
+           const enter = 13
+           if(keyCode === enter){
+             const url = `https://api.github.com/users/${value}`;
+                  fetch(url)
+                  .then((resp) => resp.json())
+                  .then((data) => console.log(data))
+                  .catch(function(error) {
+                    console.log(error);
+                  });
+           }
+           console.log(keyCode);
+         }}/>
       </div>
     );
   }
