b48afb53468cb830a43fe3047a64b67d188d4c31
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-12 18:18:55 -0200

    refactoring class

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import AppContent from '../appContent/appContent';
+import AppContent from './appContent/appContent';
 
 
 class App extends Component {
