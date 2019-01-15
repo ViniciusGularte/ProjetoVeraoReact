a306471374aa4ed55e2855791d07d37d4a119c93
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-15 08:53:59 -0200

    add pagination function part 2

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,7 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+'use strict'
+const centerRule= ({total, activePage})=> {
+  if(activePage -1 <= 0){
+    return 1;
+  }
+  return activePage - 1;
+}
 
 const pagination({total, activePage}) =>{
-  return Array.from({length:total},(_,i)=> i+1);
+  if(total <= 5){
+    return Array.from({length:total},(_,i)=> i+1);
+  }
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({length:visiblePages},(_,i) =>centerRule({total, activePage})),
+    total
+  ]
+  pages =  pages.filter((page, index, array) => array.indexOf(page) === index)
+  return pages;
 }
 export default pagination;
